0bec342f4efb6122f85135843a743dbaa982b5af
Gabriel de Jesus Santos <93293514+zxGabriel@users.noreply.github.com>
2024-01-24 14:16:32 -0300

    update 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { Estilos } from './componentes/EstilosGlobais/estilos';
+import { Card } from './componentes/Card/card';
 
 function App() {
   return (
     <div>
     <Estilos />
+    <Card>
     <h1>Freelando</h1>
+    </Card>
   </div>
   );
 }
